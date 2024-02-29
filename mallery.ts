dc84b74f3db5a8b7f1f06c886d7793324ffa8e89
Ariana <ariana.alva.aurelia@gmail.com>
2024-02-29 20:12:05 +0100

    template loading

--- mallery.ts
+++ mallery.ts
@@ -1,9 +1,21 @@
+const parser = new DOMParser();
+const resp = await fetch("./mallery.html");
+const html = await resp.text();
+const template = parser.parseFromString(html, "text/html").querySelector("template");
+
 export class Mallery extends HTMLElement {
 
     constructor() {
         super();
         console.log("Mallery instance being created");
         console.log("typescript is being compiled automatically");
+
+        if (template == null) {
+            console.log("template is null");
+            return;
+        }
+
+        this.attachShadow({ mode: "open" }).appendChild(template.content.cloneNode(true));
     }
 
 }
